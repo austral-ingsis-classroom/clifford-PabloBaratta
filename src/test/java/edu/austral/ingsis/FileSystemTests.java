721f3ee72cf94f22cafbd04de5f5d43a4bd71c43
PabloBaratta <pbaratta@mail.austral.edu.ar>
2024-06-14 12:33:41 -0300

    Changed toList() from streams tests

--- src/test/java/edu/austral/ingsis/FileSystemTests.java
+++ src/test/java/edu/austral/ingsis/FileSystemTests.java
@@ -11,13 +11,13 @@ import edu.austral.ingsis.clifford.structure.Runner;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.Test;
 
 public class FileSystemTests {
 
   private void executeTest(List<Map.Entry<String, String>> commandsAndResults) {
-    final List<String> commands = commandsAndResults.stream().map(Map.Entry::getKey).collect(Collectors.toList());
+    final List<String> commands =
+        commandsAndResults.stream().map(Map.Entry::getKey).collect(Collectors.toList());
     final List<String> expectedResult =
         commandsAndResults.stream().map(Map.Entry::getValue).collect(Collectors.toList());
 
